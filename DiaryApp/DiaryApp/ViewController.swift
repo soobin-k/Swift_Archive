94294fcaf34c5795338215f2d7d0e83111a3c61a
soobin-k <tnqls27qks@naver.com>
2022-07-21 00:59:52 +0900

    [Ch05] 일기장 삭제 기능 구현

--- DiaryApp/DiaryApp/ViewController.swift
+++ DiaryApp/DiaryApp/ViewController.swift
@@ -91,6 +91,17 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
   }
 }
 
+extension ViewController: UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let viewController = self.storyboard?.instantiateViewController(identifier: "DiaryDetailViewController") as? DiaryDetailViewController else { return}
+        let diary = self.diaryList[indexPath.row]
+        viewController.diary = diary
+        viewController.indexPath = indexPath
+        viewController.delegate = self
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+}
+
 extension ViewController: WriteDiaryViewDelegate{
     func didSelectRegister(diary: Diary) {
         self.diaryList.append(diary)
@@ -98,5 +109,12 @@ extension ViewController: WriteDiaryViewDelegate{
           $0.date.compare($1.date) == .orderedDescending
         }) // ✨ 일기 최신순 정렬
         self.collectionView.reloadData()
+    }
+}
+
+extension ViewController: DiaryDetailViewDelegate{
+    func didSelectDelete(indexPath: IndexPath) {
+        self.diaryList.remove(at: indexPath.row)
+        self.collectionView.deleteItems(at: [indexPath])
     }
 }
