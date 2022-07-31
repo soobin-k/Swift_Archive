29ec2f35756db121051f65c7d0861a349ec78c1c
soobin-k <tnqls27qks@naver.com>
2022-07-31 21:13:21 +0900

    [Ch06] 넷플릭스 스타일 영화 추천 앱 만들기

---
+++ NetflixStyleApp/NetflixStyleApp/Content.swift
@@ -0,0 +1,8 @@
+//
+//  Content.swift
+//  NetflixStyleApp
+//
+//  Created by 김수빈 on 2022/07/31.
+//
+
+import Foundation
---
+++ NetflixStyleApp/NetflixStyleApp/ContentCollectionViewHeader.swift
@@ -0,0 +1,8 @@
+//
+//  ContentCollectionViewHeader.swift
+//  NetflixStyleApp
+//
+//  Created by 김수빈 on 2022/07/31.
+//
+
+import Foundation
