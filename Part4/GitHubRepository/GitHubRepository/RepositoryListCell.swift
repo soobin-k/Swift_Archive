8bb6e46dd7cc7956b999ac2c042d06283155ef50
soobin-k <tnqls27qks@naver.com>
2022-08-16 01:00:08 +0900

    [Part4 - Ch05] 기본 UI 구성하기

---
+++ Part4/GitHubRepository/GitHubRepository/RepositoryListCell.swift
@@ -0,0 +1,28 @@
+//
+//  RepositoryListCell.swift
+//  GitHubRepository
+//
+//  Created by 김수빈 on 2022/08/16.
+//
+
+import UIKit
+import SnapKit
+
+class RepositoryListCell: UITableViewCell{
+    var repository: String?
+    
+    let nameLabel = UILabel()
+    let descriptionLabel = UILabel()
+    let starImageView = UIImageView()
+    let starLabel = UILabel()
+    let languageLabel = UILabel()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        [ nameLabel, descriptionLabel, starImageView, starLabel, languageLabel
+        ].forEach {
+            contentView.addSubview($0)
+        }
+    }
+}
