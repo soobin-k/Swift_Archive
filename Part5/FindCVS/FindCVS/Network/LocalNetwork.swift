275ed4a5646d24576ceca635776b45a2cbc62f47
soobin-k <tnqls27qks@naver.com>
2022-10-30 15:24:06 +0900

    [Part5 - Ch01] 데이터 연결하기

---
+++ Part5/FindCVS/FindCVS/Network/LocalNetwork.swift
@@ -0,0 +1,39 @@
+//
+//  LocalNetwork.swift
+//  FindCVS
+//
+//  Created by 김수빈 on 2022/10/30.
+//
+
+import RxSwift
+
+class LocalNetwork {
+    private let session: URLSession
+    let api = LocalAPI()
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func getLocation(by mapPoint: MTMapPoint) -> Single<Result<LocationData, URLError>> {
+        guard let url = api.getLocation(by: mapPoint).url else {
+            return .just(.failure(URLError(.badURL)))
+        }
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("KakaoAK 3d8ab3fb4c3d106bd5ad2dfdfe9984d5", forHTTPHeaderField: "Authorization") // header에 추가
+        
+        return session.rx.data(request: request as URLRequest)
+            .map { data in
+                do {
+                    let locationData = try JSONDecoder().decode(LocationData.self, from: data)
+                    return .success(locationData)
+                } catch {
+                    return .failure(URLError(.cannotParseResponse))
+                }
+            }
+            .catch { _ in .just(Result.failure(URLError(.cannotLoadFromNetwork))) }
+            .asSingle()
+    }
+}
--- Part5/FindCVS/FindCVS/Presentation/ LocationInformationViewModel.swift
+++ Part5/FindCVS/FindCVS/Presentation/ LocationInformationViewModel.swift
@@ -28,15 +28,47 @@ struct LocationInformationViewModel{
     let curentLocationButtonTapped = PublishRelay<Void>()
     let detailListItemSelected = PublishRelay<Int>()
     
-    let documentData = PublishSubject<[KLDocument?]>()
+    private let documentData = PublishSubject<[KLDocument]>()
     
-    init(){
+    init(model: LocationInformationModel = LocationInformationModel()){
+        //MARK: 네트워크 통신으로 데이터 불러오기
+        let cvsLocationDataResult = mapCenterPoint // api 통신
+            .flatMapLatest(model.getLocation)
+            .share()
+        
+        let cvsLocationDataValue = cvsLocationDataResult
+            .compactMap { data -> LocationData? in
+                guard case let .success(value) = data else {
+                    return nil
+                }
+                return value
+            }
+        
+        let cvsLocationDataErrorMessage = cvsLocationDataResult
+            .compactMap { data -> String? in
+                switch data {
+                case let .success(data) where data.documents.isEmpty:
+                    return """
+                    500m 근처에 이용할 수 있는 편의점이 없어요.
+                    지도 위치를 옮겨서 재검색해주세요.
+                    """
+                case let .failure(error):
+                    return error.localizedDescription
+                default:
+                    return nil // 성공이면 에러메시지 x
+                }
+            }
+        
+        cvsLocationDataValue
+            .map { $0.documents }
+            .bind(to: documentData)
+            .disposed(by: disposeBag)
+        
         //MARK: 지도 중심점 설정
         let selectDetailListItem = detailListItemSelected
             .withLatestFrom(documentData) { $1[$0]}
             .map { data -> MTMapPoint in
-               guard let data = data,
-                     let longtitue = Double(data.x),
+               guard let longtitue = Double(data.x),
                      let latitude = Double(data.y) else{
                    return MTMapPoint()
                }
@@ -54,10 +86,22 @@ struct LocationInformationViewModel{
             )
         setMapCenter = currentMapCenter
             .asSignal(onErrorSignalWith: .empty())
-        errorMessage = mapViewError.asObservable()
+        
+        errorMessage = Observable
+            .merge(
+                cvsLocationDataErrorMessage,
+                mapViewError.asObservable()
+            )
             .asSignal(onErrorJustReturn: "잠시 후 다시 시도해주세요.")
         
-        detailListCellData = Driver.just([])
+        detailListCellData = documentData
+            .map(model.documentsToCellData)
+            .asDriver(onErrorDriveWith: .empty())
+        
+        documentData
+            .map{ !$0.isEmpty}
+            .bind(to: detailListBackgroundViewModel.shoudHideStatusLabel)
+            .disposed(by: disposeBag)
         
         scrollToSelectedLocation = selectPOIItem
             .map{$0.tag}
