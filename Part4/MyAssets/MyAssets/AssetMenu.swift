61dd083632e08fecaff6749aa3ecea95e7335610
soobin-k <tnqls27qks@naver.com>
2023-06-08 01:05:17 +0900

    [Part4 - Ch01] Grid View 만들기

---
+++ Part4/MyAssets/MyAssets/AssetMenu.swift
@@ -0,0 +1,65 @@
+//
+//  AssetMenu.swift
+//  MyAssets
+//
+//  Created by 김수빈 on 2023/06/08.
+//
+
+import Foundation
+
+enum AssetMenu: String, Identifiable, Decodable {
+    case creditScore
+    case bankAccount
+    case investment
+    case loan
+    case insurance
+    case creditCard
+    case cash
+    case realEstate
+    
+    var id: String {
+        return self.rawValue
+    }
+    
+    var systemImageName: String {
+        switch self {
+        case .creditScore:
+            return "number.circle"
+        case .bankAccount:
+            return "bitcoinsign.circle"
+        case .loan:
+            return "hand.wave"
+        case .insurance:
+            return "lock.shield"
+        case .cash:
+            return "dollarsign.circle"
+        case .realEstate:
+            return "house.fill"
+        case .investment:
+            return "bitcoinsign.circle"
+        case .creditCard:
+            return "creditcard"
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .creditScore:
+            return "신용점수"
+        case .bankAccount:
+            return "계좌"
+        case .loan:
+            return "대출"
+        case .insurance:
+            return "보험"
+        case .cash:
+            return "현금영수증"
+        case .realEstate:
+            return "부동산"
+        case .investment:
+            return "투자"
+        case .creditCard:
+            return "카드"
+        }
+    }
+}
---
+++ Part4/MyAssets/MyAssets/AssetMenuButtonStyle.swift
@@ -0,0 +1,54 @@
+//
+//  AssetMenuButtonStyle.swift
+//  MyAssets
+//
+//  Created by 김수빈 on 2023/06/08.
+//
+
+import SwiftUI
+
+struct AssetMenuButtonStyle: ButtonStyle {
+    let menu: AssetMenu
+    
+    func makeBody(configuration: Configuration) -> some View {
+        return VStack {
+            Image(systemName: menu.systemImageName)
+                .resizable()
+                .frame(width: 30, height: 30)
+                .padding([.leading, .trailing], 10)
+            Text(menu.title)
+                .font(.system(size: 12, weight: .bold))
+        }
+        .padding()
+        .foregroundColor(.blue)
+        .background(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+struct AssetMenuButtonStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        HStack(spacing: 20) {
+            Button("") {
+                print("")
+            }
+            .buttonStyle(AssetMenuButtonStyle(menu: .creditScore))
+            
+            Button("") {
+                print("")
+            }
+            .buttonStyle(AssetMenuButtonStyle(menu: .bankAccount))
+            
+            Button("") {
+                print("")
+            }
+            .buttonStyle(AssetMenuButtonStyle(menu: .creditCard))
+            
+            Button("") {
+                print("")
+            }
+            .buttonStyle(AssetMenuButtonStyle(menu: .cash))
+        }
+        .background(Color.gray.opacity(0.2))
+    }
+}
