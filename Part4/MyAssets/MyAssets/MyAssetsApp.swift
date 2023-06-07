a5b3e3bcb3103b18e908310b4d6eba9d70c75068
soobin-k <tnqls27qks@naver.com>
2023-06-07 23:14:19 +0900

    [Part4 - Ch01] 탭바 만들기

---
+++ Part4/MyAssets/MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by 김수빈 on 2023/06/07.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
