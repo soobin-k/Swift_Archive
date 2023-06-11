cc03921e07e6e0937aecb3b3d278cbcf450f7299
soobin-k <tnqls27qks@naver.com>
2023-06-11 17:38:10 +0900

    [Part4 - Ch01] 자산 관리 리스트 만들기 1

---
+++ Part4/MyAssets/MyAssets/AssetSummaryView/AssetSectionHeaderView.swift
@@ -0,0 +1,30 @@
+//
+//  AssetSectionHeaderView.swift
+//  MyAssets
+//
+//  Created by 김수빈 on 2023/06/11.
+//
+
+import SwiftUI
+
+struct AssetSectionHeaderView: View {
+    let title: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.system(size: 20, weight: .bold))
+                .foregroundColor(.accentColor)
+            Divider()
+                .frame(height: 2)
+                .background(Color.primary)
+                .foregroundColor(.accentColor)
+        }
+    }
+}
+
+struct AssetSectionHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetSectionHeaderView(title: "은행")
+    }
+}
